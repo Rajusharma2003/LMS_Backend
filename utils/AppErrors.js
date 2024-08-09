8cbcc7f7212dabfd5789ffac9ebaa68524f864bf
Rajusharma2003 <rajusharma50154@gmail.com>
2024-08-09 11:06:26 +0530

    change some file name

--- utils/AppErrors.js
+++ utils/AppErrors.js
@@ -1,14 +1,3 @@
-// class AppError extends Error {
-//   constructor(message, statusCode) {
-//     super(message);
-
-//     this.statusCode = statusCode;
-
-//     Error.captureStackTrace(this, this.constructor);
-//   }
-// }
-
-// export default AppError;
 
 
 class AppError extends Error {
